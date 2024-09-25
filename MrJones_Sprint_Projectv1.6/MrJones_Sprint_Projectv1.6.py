9bd99dcc31ae0f8058815a7717632cde7ef271bc
Paul Jones <paul.tyler.jones@gmail.com>
2024-09-25 06:11:30 +0000

    Populates logic for distance functions

--- MrJones_Sprint_Projectv1.6/MrJones_Sprint_Projectv1.6.py
+++ MrJones_Sprint_Projectv1.6/MrJones_Sprint_Projectv1.6.py
@@ -1,7 +1,21 @@
 def print_menu():
-    print()
-    print()
+    print('1. Kilometers to Miles')
+    print('2. Miles to Kilometers')
+
 def km_miles():
-def miles_k():
+    km = float((input('Please enter distance in Kilometers: ')))
+    miles = km / 1.60934
+    print('Distance in miles {0}'.format(miles))
+
+def miles_km():
+    miles = float((input('Please enter distance in Miles: ')))
+    km = miles * 1.60934
+    print('Distance in Kilometers {0}'.format(km))
+
 if __name__ == '__main__':
     print_menu()
+    choice = input('Which would you like to do today?: ')
+    if choice == '1':
+        km_miles()
+    if choice == '2':
+        miles_km()
